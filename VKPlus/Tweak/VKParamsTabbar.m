a0be9b5bb813fc0954fc6aa776c20f0179e0d83d
Daniil Pashin <daniilpashin@icloud.com>
2019-08-02 18:15:10 +0300

    [f] Fix VKApp 5.21 crash

--- VKPlus/Tweak/VKParamsTabbar.m
+++ VKPlus/Tweak/VKParamsTabbar.m
@@ -15,8 +15,14 @@ CHDeclareMethod(0, void, VKMMainController, setupTabBarControllers)
     vkp_vkMainController = self;
     if ([self respondsToSelector:@selector(setNavigationControllerDelegate:)])
         self.navigationControllerDelegate = [objc_lookUpClass("DiscoverNavigationDelegate") delegateWithNavigationStats:self.main.session.navigationStats];
-    else if ([self respondsToSelector:@selector(setNavigationDelegate:)])
-        self.navigationDelegate = [objc_lookUpClass("MainNavigationDelegate") delegateWithNavigationStats:self.main.session.navigationStats];
+    else if ([self respondsToSelector:@selector(setNavigationDelegate:)]) {
+        Class delegateClass = objc_lookUpClass("MainNavigationDelegate");
+        if ([delegateClass respondsToSelector:@selector(delegateWithNavigationStats:)]) {
+            self.navigationDelegate = [delegateClass delegateWithNavigationStats:self.main.session.navigationStats];
+        } else {
+            self.navigationDelegate = [delegateClass new];
+        }
+    }
     
     [VKParamsTabbarModel rebuildTabbarItems];
     vkp_vkMainController.selectedIndex = selectedTabbarIndex;
